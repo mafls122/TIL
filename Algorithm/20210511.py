2e49735e9501fc78cda62dc6bfa5903b737dc870
JUAZZ <64843427+mafls122@users.noreply.github.com>
2021-05-13 20:04:50 +0900

    TIL Update
    TIL Update

--- Algorithm/20210511.py
+++ Algorithm/20210511.py
@@ -56,7 +56,7 @@ print('우박수의 길이 : ', maxlen)
 #         n += 1
 # 500번을 넘지 못한다. 없음
 #------------------------------------------------------------
-# 로마에서 아라비아까지 : 로마숫자를 아라비아 숫자로 바꾸기
+# 로마에서 아라비아까지 : 로마 숫자를 아라비아 숫자로 바꾸기
 
 romanDict = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
 s = 'MDCLXVI'
@@ -81,3 +81,67 @@ def romatoArabic(roman):
 res_roma = romatoArabic('XIV')
 print(res_roma)
 
+# 숫자를 로마 문자로 변경하여 출력
+
+romanDict = { 1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+
+# 1에서 9까지 출력 함수
+def romanNumber1(n):
+    str = ""
+    while n >= 1 :
+        if n == 9 :
+            str += romanDict[1] + romanDict[10]
+            n -= 9
+        elif n == 4 :
+            str += romanDict[1] + romanDict[5]
+            n -= 4
+        elif n >= 5 :
+            str += romanDict[5]
+            n -= 5
+        else :
+            for _ in range(n):
+                str += romanDict[1]
+            n -= n
+    return str
+
+for n in range(1,10):
+    print(n, ' : ', romanNumber1(n))
+
+# 100 미만의 숫자로 확장
+def romanNumber2(n):
+    str = ""
+    while n >= 50 :
+        if n >= 90:
+            str += romanDict[10] + romanDict[100]
+            n -= 90
+        else:
+            str += romanDict[50]
+            n -= 50
+    while n >= 10 :
+        if n >= 40 :
+            str += romanDict[10] + romanDict[50]
+            n -= 40
+        else:
+            str += romanDict[10]
+            n -= 10
+    return str + romanNumber1(n)
+
+for n in range(10, 100, 10):
+    print(n, ' : ', romanNumber2(n))
+
+# 숫자를 로마 문자로 변경하자!
+romanDict2 = { 1:'I', 4:'IX', 5:'V', 9:'IX', 10:'X',40:'XL', 50:'L', 90:'XC', 100:'C',400:'CD', 500:'D', 900:'CM', 1000:'M'}
+keyValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+def toRomanNumber(n):
+    str = ""
+    for i in range(len(keyValues)):
+        while n >= keyValues[i] :
+            str += romanDict2[keyValues[i]]
+            n -= keyValues[i]
+    return str
+
+# 결과 확인
+numbers = [369, 80, 29, 155, 14, 492, 348, 301, 469, 499]
+for i in numbers :
+    print(toRomanNumber(i), end=', ')
