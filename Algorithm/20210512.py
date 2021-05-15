a93e0c20585abd96de7c0b67a42a1d3c3f0ad6bb
JUAZZ <64843427+mafls122@users.noreply.github.com>
2021-05-15 23:05:42 +0900

    TIL UPDATE
    TIL UPDATE

--- Algorithm/20210512.py
+++ Algorithm/20210512.py
@@ -76,7 +76,59 @@ def eggDrop2(n):
 print(answer, eggDrop2(n))
 
 # 계란이 몇 개나 필요할까? -> log
+def countEggDrop2(n):
+    start = 1
+    end = n
+    count = 0
+    while start + 1 < end :
+        count += 1
+        mid = (start+end) // 2
+        if isSafe(mid):
+            start = mid
+        else:
+            end = mid
+    return count
 
+for i in range(1,21):
+    maxcount = 0
+    n = 2 ** i
+    for _ in range(1000):
+        answer = random.randint(1, n)
+        count = countEggDrop2(n)
+        if maxcount < count :
+            maxcount = count
+    print(n, maxcount)
 
+print()
 # 계란이 두 개 뿐이라면?, 이진탐색
+import math
 
+def twoEggsDrop(n):
+    sqrt_n = math.floor(math.sqrt(n))
+    for h in range(sqrt_n, n+1, sqrt_n):
+        if not isSafe(h):
+            break
+    for h2 in range(h - sqrt_n+1, h):
+        if not isSafe(h2):
+            return h2
+
+n = 100
+answer = random.randint(1, n)
+print(answer, twoEggsDrop(n))
+
+# 계란이 두 개 뿐일때 횟수 구하기
+def twoEggsDrop2(n):
+    count = 0
+    sqrt_n = math.floor(math.sqrt(n))
+    for h1 in range(sqrt_n, n+1, sqrt_n):
+        count += 1
+        if not isSafe(h1):
+            break
+    for h2 in range(h1 - sqrt_n+1, h1):
+        count += 1
+        if not isSafe(h2):
+            return count
+
+n = 100
+for answer in range(1,101):
+    print(answer, twoEggsDrop2(n))
